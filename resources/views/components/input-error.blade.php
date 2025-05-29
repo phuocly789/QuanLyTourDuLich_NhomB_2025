0c08920e35a700b9e65d66857faf22d85e2bfeae
ngochiep <ngochiep5640@gmail.com>
2025-05-29 17:08:51 +0700

    lỗi

--- resources/views/components/input-error.blade.php
+++
@@ -1,7 +0,0 @@
-@props(['messages'])
-
-@foreach ((array) $messages as $message)
-    <p {{ $attributes->merge(['class' => 'text-sm text-red-600']) }}>
-        {{ $message }}
-    </p>
-@endforeach
