7301d4a0e9b4733e93bb8e793b533fc83382b219
AnhTuan <anhtuan12082004@gmail.com>
2025-05-29 22:34:14 +0700

    Them footer

--- resources/views/user/service.blade.php
+++ resources/views/user/service.blade.php
@@ -19,6 +19,52 @@
 
 
 <!-- Service Start -->
+<!-- <div class="container-xxl py-5">
+    <div class="container">
+        <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+            <h1 class="text-center text-primary px-3">Dịch vụ của chúng tôi</h1>
+        </div>
+        <div class="row g-4">
+            <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item rounded pt-3">
+                    <div class="p-4">
+                        <i class="fa fa-3x fa-globe text-primary mb-4"></i>
+                        <h5>Vòng quanh thế giới</h5>
+                        <p>Đưa bạn đến với những địa điểm mới lạ trên toàn thế giới</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+                <div class="service-item rounded pt-3">
+                    <div class="p-4">
+                        <i class="fa fa-3x fa-hotel text-primary mb-4"></i>
+                        <h5>Đặt phòng khách sạn</h5>
+                        <p>Khách sạn 5* được đánh giá tốt nhất trên thế giới</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
+                <div class="service-item rounded pt-3">
+                    <div class="p-4">
+                        <i class="fa fa-3x fa-user text-primary mb-4"></i>
+                        <h5>Hướng dẫn viên du lịch</h5>
+                        <p>Thân thiện, hòa đồng và đồng hành trong suốt hành trình của bạn</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
+                <div class="service-item rounded pt-3">
+                    <div class="p-4">
+                        <i class="fa fa-3x fa-cog text-primary mb-4"></i>
+                        <h5>Quản lý sự kiện</h5>
+                        <p>Những sự kiện, hoạt động trong suốt hành trình của bạn</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div> -->
 <div class="container-xxl py-5">
     <div class="container">
         <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
